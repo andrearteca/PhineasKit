0bd446c9b74ca38e54259b89c317f38c3faf3505
andrearteca <andre.arteca1@gmail.com>
2023-11-10 14:49:35 -0300

    Initial Commit

---
+++ Sources/PhineasKit/PhineasKit.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+public struct VerdadeView: View{
+    
+    public init(){}
+    
+    public var body: some View{
+        Text("Oi")
+    }
+    
+}
+
+#Preview {
+    VerdadeView()
+}
